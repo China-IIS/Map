57bea640830e479370f544eeaf78f00c2997ae4d
MEMZ-JJY <155820921+MEMZ-JJY@users.noreply.github.com>
2025-07-25 16:29:23 +0800

    Update script.js
    fix map show info err

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', () => {
     let navbar = document.getElementById('navbar');
-    let navbarToggle = document.getElementById('navbar-toggle');
-
-    navbarToggle.addEventListener('click', () => {
-        navbar.classList.toggle('list-open');
-        navbarToggle.classList.toggle('selected');
-    });
+    let navbarToggle = document.getElementById('navbar-bars');
+    if (navbar && navbarToggle) {
+        navbarToggle.addEventListener('click', () => {
+            navbar.classList.toggle('list-open');
+            navbarToggle.classList.toggle('selected');
+        });
+    } else {
+        console.warn("Navbar or Navbar Toggle element not found in DOM.");
+    }
 });
